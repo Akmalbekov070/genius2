a453e4df71bd880159793ed1f5a38cea68312c8a
Akmalbekov070 <akmalbekov2022@gmail.com>
2025-01-22 15:20:06 +0300

    heDER CHANGED CODE

--- src/components/HeaderPage/HeaderPage.jsx
+++ src/components/HeaderPage/HeaderPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Box, HStack, Image, Text } from '@chakra-ui/react';
+import { Box, Button, HStack, Image, Text } from '@chakra-ui/react';
 import Link from 'next/link';
+import { GrInstagram } from 'react-icons/gr';
+import { RiTelegramLine } from 'react-icons/ri';
 
 export default function HeaderPage() {
 	return (
-		<Box w={'full'} h={24} bg={'blue.700'} display={'flex'} alignItems={'center'} justifyContent={'space-between'} px={20}>
+		<Box
+			w={'full'}
+			h={24}
+			bg={'blue.700'}
+			display={'flex'}
+			alignItems={'center'}
+			justifyContent={'space-between'}
+			px={16}
+			color={'white'}
+		>
 			{/* left header  */}
 			<Link href={'/'}>
 				<HStack>
@@ -13,7 +24,8 @@ export default function HeaderPage() {
 						src='https://i.postimg.cc/SxydV0n1/removal-ai-e2c59107-02d4-4e7b-a4bc-dd5d9d16f024-photo-2024-11-04-10-22-39.png'
 						alt='Image Genius'
 					/>
-					<Text w={'140px'} fontStyle={'-moz-initial'} fontFamily={'mono'} fontSize={'xl'}>
+					<Box w={'1px'} h={16} bg={'white'}></Box>
+					<Text w={'140px'} fontStyle={'-moz-initial'} fontFamily={'mono'} fontSize={'lg'} pl={2}>
 						Genius Academy
 					</Text>
 				</HStack>
@@ -36,6 +48,11 @@ export default function HeaderPage() {
 					Join a Group
 				</Link>
 				{/* clerk this */}
+			</HStack>
+			<HStack gap={3} cursor={'pointer'}>
+				<GrInstagram size={30} className='hover:text-yellow-500' />
+				<RiTelegramLine size={37} className='hover:text-yellow-500' />
+				<Button bg={'yellow.400'}>Join a Group</Button>
 			</HStack>
 		</Box>
 	);
--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import HeaderPage from '../HeaderPage/HeaderPage';
-import { Box } from '@chakra-ui/react';
+import { Box, Image } from '@chakra-ui/react';
 
 export default function HomePage() {
 	return (
 		<Box>
 			<Box>
 				{/* HeaderPage */}
-				<HeaderPage />;
+				<HeaderPage />
 			</Box>
+			<Image
+				src='https://avatars.mds.yandex.net/i?id=eaed6936a12d380e0ccdbbe1da42a0b17fb9165e-8497133-images-thumbs&n=13'
+				alt=''
+			/>
 		</Box>
 	);
 }
