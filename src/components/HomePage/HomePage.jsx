aafe6d5226b2bd9d9bd0b9c01218c93e43d88503
Akmalbekov070 <akmalbekov2022@gmail.com>
2025-01-24 15:03:57 +0300

    homepage fon upload

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import HeaderPage from '../HeaderPage/HeaderPage';
-import { Box, Image } from '@chakra-ui/react';
+import { Box, Heading, Image } from '@chakra-ui/react';
 
 export default function HomePage() {
 	return (
@@ -9,10 +9,19 @@ export default function HomePage() {
 				{/* HeaderPage */}
 				<HeaderPage />
 			</Box>
-			<Image
-				src='https://avatars.mds.yandex.net/i?id=eaed6936a12d380e0ccdbbe1da42a0b17fb9165e-8497133-images-thumbs&n=13'
-				alt=''
-			/>
+			<Box w={'full'} position={'absolute'} top={24} left={0}>
+				<Image
+					w={'full'}
+					h={'100%'}
+					src='https://avatars.mds.yandex.net/i?id=eaed6936a12d380e0ccdbbe1da42a0b17fb9165e-8497133-images-thumbs&n=13'
+					alt=''
+				/>
+			</Box>
+			{/* black fon */}
+			<Box w={'full'} h={'760px'} bg={'blackAlpha.800'} position={'absolute'} top={24} left={0}></Box>
+			<Box position={'relative'} zIndex={24}>
+				<Heading>Salom</Heading>
+			</Box>
 		</Box>
 	);
 }
