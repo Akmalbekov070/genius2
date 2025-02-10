a9970cece0223e580d36ca48755aed4150575831
Akmalbekov070 <akmalbekov2022@gmail.com>
2025-02-10 14:59:22 +0300

    commit second commit

--- src/components/MenuContactPage/MenuContactPage.jsx
+++ src/components/MenuContactPage/MenuContactPage.jsx
@@ -1,5 +1,31 @@
+import { Box, Heading, Image, Text } from '@chakra-ui/react';
 import React from 'react';
 
 export default function MenuContactPage() {
-	return <div>MenuContactPage</div>;
+	return (
+		<Box
+			w={'full'}
+			bg={'black'}
+			display={'flex'}
+			justifyContent={'space-between'}
+			alignItems={'center'}
+			px={{ xl: '150px', base: '50px' }}
+			pt={10}
+		>
+			<Box>
+				<Heading fontFamily={'serif'} color={'white'} fontSize={'5xl'} letterSpacing={10}>
+					Genius
+				</Heading>
+				<Text color={'yellow.600'} fontSize={'2xl'} fontFamily={'serif'}>
+					Academy
+				</Text>
+				<Text color={'white'} fontSize={'xl'} fontFamily={'serif'}>
+					+998(91).313.89.89
+				</Text>
+        <Heading>
+					
+				</Heading>
+			</Box>
+		</Box>
+	);
 }
--- src/components/PrisecPage/PricesPage.jsx
+++ src/components/PrisecPage/PricesPage.jsx
@@ -64,8 +64,6 @@ export default function PricesPage() {
 			<ContactPage />
 			{/* mapsPahe----- */}
 			<MapsPage />
-			{/* MenuPageContact----- */}
-			<MenuContactPage />
 		</Box>
 	);
 }
